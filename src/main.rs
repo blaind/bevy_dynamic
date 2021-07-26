4e0071be9f8c3c6e703d8ffa37eabce7c3580374
Mika Vatanen <blaind@blaind.net>
2021-07-26 15:39:03 +0300

    Run in one repository

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, tasks::AsyncComputeTaskPool};
 
 fn main() {
     App::build()
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
-        .add_startup_system(bevy_lib::test_system.system())
+        .add_startup_system(test_system.system())
         .run();
 }
 
@@ -38,4 +38,21 @@ fn setup(
         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..Default::default()
     });
+}
+
+pub fn test_system(mut commands: Commands, thread_pool: Res<AsyncComputeTaskPool>) {
+    let task = thread_pool.spawn(async move {
+        async_compat::Compat::new(async {
+            do_request().await;
+        })
+        .await;
+    });
+
+    commands.spawn().insert(task);
+}
+
+async fn do_request() {
+    println!("Doing request...");
+    let req = reqwest::get("http://localhost:12345").await;
+    println!("Request success: {:?}", req.is_ok());
 }
